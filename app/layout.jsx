dc883454d39725287690965577643b58921b8365
baltej223 <baltej963@gmail.com>
2025-08-11 00:31:34 +0530

    Revert "Some bugs fixed"
    This reverts commit 56d1656f73bdfbfc68d724baea316b0ca74511d3.

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,26 +1,25 @@
-"use client";
 import "./globals.css";
 import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
 import Head from 'next/head';
-import { usePathname } from 'next/navigation';
+import { useRouter } from 'next/router';
 
 export default function RootLayout({ children }) {
-    const pathname = usePathname();
-    const canonicalUrl = `https://posts.baltej.me${pathname}`;
+    const router = useRouter();
+    const canonicalUrl = `https://posts.baltej.me${router.asPath.split('?')[0]}`;
 
-    return (
-        <html lang="en">
-            <Head>
-                <link rel="canonical" href={canonicalUrl} />
-            </Head>
-            <body className="min-h-screen flex flex-col">
-                <Navbar />
-                <main className="flex-grow pt-10 pl-10 pr-10 md:pl-30 md:pr-30 lg:pl-100 lg:pr-100">
-                    {children}
-                </main>
-                <Footer />
-            </body>
-        </html>
-    );
+  return (
+    <html lang="en">
+      <Head>
+        <link rel="canonical" href={canonicalUrl} />
+      </Head>
+      <body className="min-h-screen flex flex-col">
+        <Navbar />
+        <main className="flex-grow pt-10 pl-10 pr-10 md:pl-30 md:pr-30 lg:pl-100 lg:pr-100">
+          {children}
+        </main>
+        <Footer />
+      </body>
+    </html>
+  );
 }
